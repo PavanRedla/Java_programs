93794f1d2519e1392bc0e067e6a9fec92935d507
PavanRedla <pavankalyanredla42@gmail.com>
2024-04-25 16:07:08 +0530

    Exception

--- Current Running/App_Exception1/src/maccess/DemoException1.java
+++ Current Running/App_Exception1/src/maccess/DemoException1.java
@@ -6,11 +6,20 @@ public class DemoException1
 	public static void main(String args[])
 	{
 		Scanner s = new Scanner(System.in);
-		
-		System.out.println("Enter the Marks of Java: ");
-		int mk = Integer.parseInt(s.nextLine());
-		System.out.println("Marks in Java: "+mk);
-		s.close();
+		try
+		{
+			System.out.println("Enter the Marks of Java: ");
+			int mk = Integer.parseInt(s.nextLine());
+			System.out.println("Marks in Java: "+mk);
+		}
+		catch(NumberFormatException nfe)
+		{
+			System.out.println("Enter Integer value only..");
+		}
+		finally
+		{
+			s.close();
+		}
 	}
 
 }
